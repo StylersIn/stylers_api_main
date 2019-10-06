2d726318a8642a67661a6cfeb124a2bf1ae18215
buka4chocksy <buka4chocksy@gmail.com>
2019-10-06 21:31:28 +0100

    modified authmiddleware

--- app/Middleware/AuthMiddleware.js
+++ app/Middleware/AuthMiddleware.js
@@ -1,16 +1,13 @@
 var authService = require('../Service/authService');
 var BaseRepository = require('../Repository/BaseRepository');
-var Client = require('../Model/clients');
-var Stylist = require('../Model/stylists');
-var ClientRepo = new BaseRepository(Client);
-var StylistRepo = new BaseRepository(Stylist);
+var User = require('../Model/user');
+var UserRepo = new BaseRepository(User);
 
 exports.authenticate = function(req,res,next){
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
     if(token){
         authService.verifyToken(token, userType = '').then(decoded =>{
-            if(decoded.userType === 'client'){
-                ClientRepo.getSingleBy({publicId:decoded.publicId}, '').then(data =>{
+                UserRepo.getSingleBy({publicId:decoded.publicId}, '').then(data =>{
                     if(data == null){
                         res.status(401).send({success:false, message: "User does not exist" });
                     }else{
@@ -29,25 +26,6 @@ exports.authenticate = function(req,res,next){
                         next();
                     }
                 })
-            }else if(decoded.userType === 'stylist'){
-                StylistRepo.getSingleBy({publicId:decoded.publicId},'').then(data =>{
-                    if(data == null){
-                        res.status(401).send({success:false , message: "User does not exist"});
-                    }else{
-                        req.auth = {
-                            userId:data.userId,
-                            publicId:data.publicId,
-                            userType:decoded.userType,
-                            email:decoded.email,
-                            fullName:decoded.fullName,
-                            Id:data._id
-                        }
-                        console.log(req.auth , 'auth details')
-                        res.locals.response = {data:decoded , message:"" , success:true};
-                        next();
-                    }
-                });
-            }
         }).catch(err =>{
             res.status(401).send({ success: false, message: "Invalid token", data: err });
  
