b1563238efb307a83865afbd1c3ed3d9ec1a8b0f
buka4chocksy <buka4chocksy@gmail.com>
2019-10-06 21:34:45 +0100

    modified auth service

--- app/Service/authService.js
+++ app/Service/authService.js
@@ -1,13 +1,9 @@
 var BaseRepository =  require('../Repository/BaseRepository');
 var User = require('../Model/user');
-var Client = require('../Model/clients');
-var Stylers = require('../Model/stylists')
 var mailer = require('../Middleware/mailer')
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 var UserRepo = new BaseRepository(User);
-var ClientRepo = new BaseRepository(Client);
-var StylersRepo = new BaseRepository(Stylers);
 var secret = process.env.Secret;
 exports.RegisterUser = (Options)=>{
   return new Promise((resolve, reject)=>{
@@ -26,29 +22,24 @@ exports.RegisterUser = (Options)=>{
          if(exists){
          reject({success: false , message:'Sorry user already exists'});
          }else{
-            UserRepo.add(u).then((user)=>{
-                var D  = {
-                    fullName:Options.fullName,
-                    email:Options.email,
-                    userId:user._id,
-                    publicId:user.publicId,
-                    phoneNumber:Options.phoneNumber
-                }
-            u.userType == 'stylist' ? StylersRepo.add(D) :   ClientRepo.add(D)
-
-            }).then(()=>{
-                mailer.UserAdded(u.email,u.fullName ,u.statusCode, function(err , sent){
-                    if(err){
-                        resolve({ success:false , message:'Registration error'});
-                    }else{
-                        resolve({ success:true , message:'Registration Successful'});
-                    }
-                })
-       
-            }).catch((err)=>{
-                reject({success: false , message:'Error Registering user ', data:err});
-            })
+             UserRepo.add(u).then(created =>{
+                 if(created){
+                    mailer.UserAdded(u.email,u.fullName ,u.statusCode, function(err , sent){
+                        if(err){
+                            resolve({ success:false , message:'Registration error'});
+                        }else{
+                            resolve({ success:true , message:'Registration Successful'});
+                        }
+                    })
+                 }else{
+                    resolve({success: false , message:'User SignUp was not successfull'});
+ 
+                 }
+             })
+           
          }
+     }).catch(err =>{
+         reject(err);
      })
   
   })
@@ -94,18 +85,11 @@ exports.authenticateuser = authenticateuser
 function getUserDetail(user,Id){
     return new Promise((resolve, reject)=>{
         //console.log('this is user detail', user.status);
-        if(user.userType == 'client'){
-            ClientRepo.getSingleBy({publicId:Id}, {"_id" : 0, "__v" : 0}).then(data =>{
+        UserRepo.getSingleBy({publicId:Id}, {"_id" : 0, "__v" : 0}).then(data =>{
                 var specificUserDetail = {email: user.email,   phone: user.phoneNumber, userType : user.userType, publicId : user.publicId};
                 resolve(specificUserDetail);
             }).catch(error => reject(error))
-        }else{
-            StylersRepo.getSingleBy({publicId:Id}, {"_id" : 0, "__v" : 0}).then(data =>{
-                var specificUserDetail = {email: user.email,   phone: user.phoneNumber, userType : user.userType, publicId : user.publicId};
-                userdetail = {...data.toObject(),...specificUserDetail};
-                resolve(userdetail);
-            }).catch(error => reject(error));
-        }
+    
     })
 }
 
