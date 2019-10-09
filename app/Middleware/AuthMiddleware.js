ae9bc1c0737a5ad607e7387bd606245028ae4490
buka4chocksy <buka4chocksy@gmail.com>
2019-10-09 17:44:07 +0100

    created auth middleware and added stylers auth middleware

--- app/Middleware/AuthMiddleware.js
+++ app/Middleware/AuthMiddleware.js
@@ -1,7 +1,10 @@
-var authService = require('../Service/authService');
+var authService = require('../Service/UserauthService');
+var StylerService = require('../Service/StylersauthService');
 var BaseRepository = require('../Repository/BaseRepository');
 var User = require('../Model/user');
+var styler = require('../Model/stylers');
 var UserRepo = new BaseRepository(User);
+var StylerRepo = new BaseRepository(styler);
 
 exports.authenticate = function(req,res,next){
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
@@ -21,6 +24,35 @@ exports.authenticate = function(req,res,next){
                         }
                         if(userType !=='' && userType.length > 0){
                             if(userType != decoded.userType)res.status(401).send({success:false , message: "Invalid token", data:err})
+                        }
+                        res.locals.response = {data: decoded , message:"", success:true};
+                        next();
+                    }
+                })
+        }).catch(err =>{
+            res.status(401).send({ success: false, message: "Invalid token", data: err });
+ 
+        })
+    }else{
+        res.status(401).send({ success: false, message: "No token provided" });
+ 
+    }
+}
+
+exports.StylerAuthenticate = function(req,res,next){
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if(token){
+        StylerService.verifyToken(token, userType = '').then(decoded =>{
+            StylerRepo.getSingleBy({publicId:decoded.publicId}, '').then(data =>{
+                    if(data == null){
+                        res.status(401).send({success:false, message: "User does not exist" });
+                    }else{
+                        req.auth ={
+                            userId:data.userId,
+                            publicId:data.publicId,
+                            email:decoded.email,
+                            fullName:decoded.fullName,
+                            Id:data._id
                         }
                         res.locals.response = {data: decoded , message:"", success:true};
                         next();
