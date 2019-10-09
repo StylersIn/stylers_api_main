ced33af098594ed631e16b19cd6e08b4870f55f4
buka4chocksy <buka4chocksy@gmail.com>
2019-10-09 17:47:06 +0100

    created  stylers model

---
+++ app/Model/stylers.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var stylersSchema = new Schema({
+    fullName:{type:String , required:true},
+    email:{type:String , required:true, unique:true},
+    phoneNumber:{type:String , required:true},
+    publicId:{type: mongoose.Types.ObjectId},
+    address:{type:String , required:true},
+    description:{type:String , required:true},
+    gender:{type:String , required:true},
+    password:{type:String, required:true},
+    IsVerified:{type:Boolean},
+    imageUrl: {type: String, default:''},
+    imageID: {type: String, default: ''},
+    services: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'services', autopopulate: true }],
+    CreatedAt:{type:Date},
+
+
+})
+
+module.exports = mongoose.model('stylers', stylersSchema);
