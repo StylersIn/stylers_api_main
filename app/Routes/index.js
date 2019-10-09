8a4fabbf808762c81f2fc26a0f0d9746f23b08a5
buka4chocksy <buka4chocksy@gmail.com>
2019-10-09 17:49:02 +0100

    added stylers auth routes

--- app/Routes/index.js
+++ app/Routes/index.js
@@ -1,7 +1,9 @@
-var AuhtRoutes = require('./AuthRoutes');
+var AuhtRoutes = require('./UserAuthRoutes');
+var StylerAuhtRoutes = require('./StylersAuthRoute');
 
 module.exports = function(router){
     router.use('/auth',AuhtRoutes())
+    router.use('/StylerAuth',StylerAuhtRoutes())
 
 
  return router;
