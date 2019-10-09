0b6cbd8c0ebd125f3efe39460e1eca07ea85284b
buka4chocksy <buka4chocksy@gmail.com>
2019-10-09 17:42:45 +0100

    created user auth controllers

---
+++ app/Controller/UserAuthController.js
@@ -0,0 +1,73 @@
+var userService = require('../Service/UserauthService');
+var cloudinary = require('../Middleware/cloudinary')
+var mongoose = require('mongoose');
+var rand = require('random-number');
+var gen = rand.generator({
+  min:  1000
+, max:  100000
+, integer: true
+})
+
+module.exports = function authController(){
+    this.register = (req,res, next)=>{
+        var Options ={
+            fullName:req.body.fullname,
+            publicId: mongoose.Types.ObjectId(),
+            statusCode: gen(),
+            gender:req.body.gender,
+            email:req.body.email,
+            phoneNumber:req.body.phonenumber,
+            password:req.body.password,
+            userType:req.body.usertype,
+            status:false
+        }
+        userService.RegisterUser(Options).then((data)=>{
+            res.json(data);
+        }).catch((err)=>{
+            res.json(err);
+        })
+    }
+
+    this.authenticate = function(req, res, next){
+        var username = req.body.username
+        var password = req.body.password
+        userService.authenticateuser(username,password)
+        .then(data => res.status(200).send(data))
+        .catch(err => res.status(500).send(err));
+    }
+
+    this.VerifyUser = function(req, res){
+        var email = req.body.email
+        var Token = req.body.token
+        userService.verifyAccount(email , Token)
+        .then(data => res.status(200).send(data))
+        .catch(err => res.status(500).send(err));
+    }
+
+    this.updateClientProfile = async (req,res) => {
+        console.log("checking file", (req.file != null && req.file !== undefined));
+         var requestDetails = {
+             image: (req.file != null && req.file !== undefined) ? req.file.path : null
+         };
+        
+         console.log("file detail recieved", requestDetails.image);
+         if(req.image !== null && req.file !== undefined){
+           await cloudinary.uploadToCloud(requestDetails.image).then((img)=>{
+             console.log("Cloudinary details recieved", img.url);
+             requestDetails.imageUrl = img.url;
+             requestDetails.imageID = img.ID;
+             return requestDetails;
+         });
+     }
+ 
+     console.log("calling outside await")
+     userService.updateProfile(req.auth.publicId,requestDetails)
+         .then(data => {
+             res.status(200).send(data);
+         })
+         .catch(err => {
+             res.status(500).send(err);
+         });
+     };
+  
+}
