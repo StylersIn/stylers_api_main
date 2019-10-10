dd1ef92f8f91bc438467dec5692c4d5886a15e08
buka4chocksy <buka4chocksy@gmail.com>
2019-10-10 17:33:52 +0100

    added service index route

--- app/Routes/index.js
+++ app/Routes/index.js
@@ -1,10 +1,10 @@
 var AuhtRoutes = require('./UserAuthRoutes');
 var StylerAuhtRoutes = require('./StylersAuthRoute');
-
+var ServicesRoutes = require('./ServicesRoutes')
 module.exports = function(router){
     router.use('/auth',AuhtRoutes())
     router.use('/StylerAuth',StylerAuhtRoutes())
-
+    router.use('/services' , ServicesRoutes())
 
  return router;
 }
