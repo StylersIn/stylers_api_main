c690ecb47125c5952c0eb8b3edfe9ddd9df915dc
buka4chocksy <buka4chocksy@gmail.com>
2019-10-10 17:34:17 +0100

    created service route

---
+++ app/Routes/ServicesRoutes.js
@@ -0,0 +1,17 @@
+var ServicesauthController = require('../Controller/ServicesController');
+var middleware = require('../Middleware/AuthMiddleware');
+var multer = require('../Middleware/multer')
+var router = require('express').Router();
+module.exports = function(){
+    const servicesauthCtrl = new ServicesauthController();
+    router.post('/', multer.upload.single('image') , servicesauthCtrl.CreateService)
+    router.get('/search',servicesauthCtrl.SearchServices);
+    router.get('/:pagesize/:pagenumbe', servicesauthCtrl.GetAllServices)
+    router.get('/:id', servicesauthCtrl.GetServiceById)
+    router.delete('/:id', servicesauthCtrl.DeleteService)
+    router.put('/:id', multer.upload.single('image') , servicesauthCtrl.UpdateService)
+    
+
+
+    return router;
+}
