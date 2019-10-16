89352155e4e54e698ed8e1cf3ecfc00a5f54347c
buka4chocksy <buka4chocksy@gmail.com>
2019-10-16 16:13:38 +0100

    created booking model

---
+++ app/Model/booking.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mongoosastic = require('mongoosastic')
+var BookingSchema = new Schema({
+    userId:{ type: mongoose.SchemaTypes.ObjectId, ref: 'user', autopopulate: true },
+    stylerId:{ type: mongoose.SchemaTypes.ObjectId, ref: 'stylers', autopopulate: true },
+    serviceId:[{ type: mongoose.SchemaTypes.ObjectId, ref: 'services', autopopulate: true }],
+    scheduledDate:{ type: Date, default: new Date() },
+    numberOfAdults:{type:Number},
+    numberOfChildren:{type:Number},
+    location:{type: String , required:true},
+    TotalAmount:{type: String},
+    CreatedAt:{type:Date},
+
+
+})
+BookingSchema.plugin(mongoosastic)
+
+module.exports = mongoose.model('booking', BookingSchema); 
