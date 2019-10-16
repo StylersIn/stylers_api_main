8e355a0d2310918532a5b17adde413e23ca3eaa1
buka4chocksy <buka4chocksy@gmail.com>
2019-10-16 16:16:12 +0100

    modified get all services routes

--- app/Routes/ServicesRoutes.js
+++ app/Routes/ServicesRoutes.js
@@ -6,12 +6,10 @@ module.exports = function(){
     const servicesauthCtrl = new ServicesauthController();
     router.post('/', multer.upload.single('image') , servicesauthCtrl.CreateService)
     router.get('/search',servicesauthCtrl.SearchServices);
-    router.get('/:pagesize/:pagenumbe', servicesauthCtrl.GetAllServices)
+    router.get('/:pagesize/:pagenumber', servicesauthCtrl.GetAllServices)
     router.get('/:id', servicesauthCtrl.GetServiceById)
     router.delete('/:id', servicesauthCtrl.DeleteService)
     router.put('/:id', multer.upload.single('image') , servicesauthCtrl.UpdateService)
     
-
-
     return router;
 }
