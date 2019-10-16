8f36aa0544ae6cbc02516cc06a15423f1a03df50
buka4chocksy <buka4chocksy@gmail.com>
2019-10-16 16:15:39 +0100

    created booking index

--- app/Routes/index.js
+++ app/Routes/index.js
@@ -1,10 +1,12 @@
 var AuhtRoutes = require('./UserAuthRoutes');
 var StylerAuhtRoutes = require('./StylersAuthRoute');
 var ServicesRoutes = require('./ServicesRoutes')
+var BookingRoutes = require('./BookingRoutes')
 module.exports = function(router){
     router.use('/auth',AuhtRoutes())
     router.use('/StylerAuth',StylerAuhtRoutes())
     router.use('/services' , ServicesRoutes())
+    router.use('/booking' , BookingRoutes())
 
  return router;
 }
