2e1e52f987a779c43f348056bfd63ec3be1f4c8f
buka4chocksy <buka4chocksy@gmail.com>
2019-11-01 11:18:59 +0100

    added styler , favourite and review routes

--- app/Routes/StylersAuthRoute.js
+++ app/Routes/StylersAuthRoute.js
@@ -8,9 +8,12 @@ module.exports = function(){
     router.post('/authenticate', StylerauthCtrl.authenticate);
     router.put('/updateService/:id', StylerauthCtrl.UpdateServices);
     router.get('/stylers/:pagesize/:pagenumber', StylerauthCtrl.GetStylers);
+    router.get('/styler/:id', StylerauthCtrl.GetStyler);
     router.post('/addService/:id', StylerauthCtrl.AddServices);
+    router.post('/favourite/:id', middleware.authenticate , StylerauthCtrl.favouriteStylerService);
     router.post('/update', middleware.StylerAuthenticate , multer.upload.single('image') , StylerauthCtrl.updateClientProfile)
     router.get('/:service/:pagesize/:pagenumber', StylerauthCtrl.GetStylersByServices);
+    router.post('/review/:id', middleware.authenticate , StylerauthCtrl.StylerReview);
 
 
 
