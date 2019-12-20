b26d2037d38da418b9371be30c1121729f14d9d4
buka4chocksy <buka4chocksy@gmail.com>
2019-12-20 16:08:54 +0100

    payment model created

--- app/Model/payment.js
+++ app/Model/payment.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var paymentSchema = new Schema({
-    fullName:{type:String , required:true},
+    amount:{type:Number , required:true},
     userId: { type: mongoose.SchemaTypes.ObjectId, ref: 'user', autopopulate: true },
+    stylerId:{ type: mongoose.SchemaTypes.ObjectId, ref: 'stylers', autopopulate: true },
+    appointmentId:{ type: mongoose.SchemaTypes.ObjectId, ref: 'booking', autopopulate: true },
+    refNumber:{type:Number , required:true},
     CreatedAt:{type:Date}
 
 
