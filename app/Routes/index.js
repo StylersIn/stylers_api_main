4c605eed8d4e2b45e42c07443b86d5ea4cafa1b4
buka4chocksy <buka4chocksy@gmail.com>
2019-12-20 16:11:54 +0100

    route name changed

--- app/Routes/index.js
+++ app/Routes/index.js
@@ -1,13 +1,13 @@
 var AuhtRoutes = require('./UserAuthRoutes');
 var StylerAuhtRoutes = require('./StylersAuthRoute');
 var ServicesRoutes = require('./ServicesRoutes');
-var BookingRoutes = require('./AppointmentRoutes');
+var AppointmentRoutes = require('./AppointmentRoutes');
 var ContactRoutes = require("./contactRoutes");
 module.exports = function(router){
     router.use('/auth',AuhtRoutes())
     router.use('/StylerAuth',StylerAuhtRoutes())
     router.use('/services' , ServicesRoutes())
-    router.use('/booking' , BookingRoutes())
+    router.use('/appointment' , AppointmentRoutes())
     router.use('/help', ContactRoutes())
  return router;
 }
