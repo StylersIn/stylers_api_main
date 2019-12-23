9f85af02b5ecae8d7fde70313cb2399906c71377
buka4chocksy <buka4chocksy@gmail.com>
2019-12-23 10:29:35 +0100

    added payment index routes

--- app/Routes/index.js
+++ app/Routes/index.js
@@ -2,12 +2,14 @@ var AuhtRoutes = require('./UserAuthRoutes');
 var StylerAuhtRoutes = require('./StylersAuthRoute');
 var ServicesRoutes = require('./ServicesRoutes');
 var AppointmentRoutes = require('./AppointmentRoutes');
+var PaymentRoutes = require('./paymentRoutes')
 var ContactRoutes = require("./contactRoutes");
 module.exports = function(router){
     router.use('/auth',AuhtRoutes())
     router.use('/StylerAuth',StylerAuhtRoutes())
     router.use('/services' , ServicesRoutes())
     router.use('/appointment' , AppointmentRoutes())
+    router.use('/pay', PaymentRoutes())
     router.use('/help', ContactRoutes())
  return router;
 }
